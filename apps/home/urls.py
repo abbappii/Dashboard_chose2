8c2f30963ac794e9328c7092ed5efdb26b82cab8
abbappii <bappi142434@gmail.com>
2022-02-17 21:24:02 +0600

    button added for check

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,7 +4,7 @@ from apps.home import views
 urlpatterns = [
 
     path('', views.index, name='home'),
-    path('user_create/', views.user_create, name='user-create'),
+    path('user-create/', views.user_create, name='user-create'),
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
 ]
