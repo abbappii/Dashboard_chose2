f9de0f9c9bca3391abc47ee2221880598af2029e
abbappii <bappi142434@gmail.com>
2022-02-19 17:14:40 +0600

    permissson of admin only

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -4,7 +4,9 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
-from . models import UserProfile
+from . models import UserProfile,Roles
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(Roles)
+
